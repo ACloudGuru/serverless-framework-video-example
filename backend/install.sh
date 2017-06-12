648750f43c181549166fb66464059b55d1722603
Daniel Parker <dparker.tech@gmail.com>
2017-06-11 23:55:20 -0500

    Initial commit

---
+++ backend/install.sh
@@ -0,0 +1,1 @@
+for d in ./*/ ; do (cd "$d" && npm install); done
---
+++ website/js/config.example.js
@@ -0,0 +1,8 @@
+var configConstants = {
+    auth0: {
+        domain: 'AUTH0 DOMAIN',
+        clientId: 'AUTH0 CLIENT ID'
+    },
+    apiBaseUrl: 'YOUR_API_BASE_URL_HERE_NO_TRAILING_SLASH',
+    firebaseUrl: 'YOUR FIREBASE URL GOES HERE WITH NO TRAILING SLASH'
+};
